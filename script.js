c48c856d20348fda9e515b78ae06a6d6b6937501
chrissundal <christoffersj@hotmail.com>
2024-09-03 07:13:10 +0200

    dd

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-function show(color1, color2, color3) {
-    document.getElementById('app').innerHTML = /*HTML*/`
+
+updateView();
+function updateView(color1, color2, color3) {
+    let html = /*HTML*/`
+    <h1>Trafikklys</h1>
+    <div class="buttons">
+        <button onclick="updateView('gray', 'gray', 'green')">Grønn</button>
+        <button onclick="updateView('gray','yellow', 'gray')">Gul</button>
+        <button onclick="updateView('red', 'gray', 'gray')">Rød</button>
+        <button onclick="updateView('red','yellow', 'gray')">Gul Rød</button>
+        <button onclick="autoLightRed()">Auto</button>
+        <button onclick="uteAvDrift()">Ute av drift</button>
+        <button onclick="turnOff()">Slå av</button>
+    </div>
     <div id="trafikkLys">
         <div class="lys" style="background-color: ${color1};"></div>
         <div class="lys" style="background-color: ${color2};"></div>
         <div class="lys" style="background-color: ${color3};"></div>            
     </div>
         `;
+    document.getElementById('app').innerHTML = html;
 }
+
 function autoLightGreen() {
-    show('gray', 'gray', 'green');
+    updateView('gray', 'gray', 'green');
     setTimeout(autoLightYellow, 2000);
 }
 function autoLightYellow() {
-    show('gray', 'yellow', 'gray');
+    updateView('gray', 'yellow', 'gray');
     setTimeout(autoLightRed, 2000);
 
 }
 function autoLightRed() {
-    show('red', 'gray', 'gray');
+    updateView('red', 'gray', 'gray');
     setTimeout(autoLightRedYellow, 1000);
 
 }
 function autoLightRedYellow() {
-    show('red', 'yellow', 'gray')
+    updateView('red', 'yellow', 'gray')
     setTimeout(autoLightGreen, 1000)
 }
 function uteAvDrift() {
-    show('gray', 'gray', 'gray')
+    updateView('gray', 'gray', 'gray')
     setTimeout(uteAvDriftLys, 500);
 
 }
 function uteAvDriftLys() {
-    show('gray', 'yellow', 'gray');
+    updateView('gray', 'yellow', 'gray');
     setTimeout(uteAvDrift, 500);
 }
 function turnOff() {
